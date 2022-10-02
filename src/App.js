4796a36766402cd150fd2e5d5dfbe3e3187d6033
quentinberry <34132543+quentinberry@users.noreply.github.com>
2022-10-02 17:54:05 +0200

    not wokring

--- src/App.js
+++ src/App.js
@@ -5,14 +5,28 @@ const readWholeDB = () => {
  console.log(passwordJson);
 };
 
+const searchPasswordByTitle = term => {
+  let passwordArray = [];
+  for(let i = 0; i <= passwordJson.length; i++){
+    passwordArray = passwordJson[i].Titel === term ?
+      passwordArray.push(passwordJson[i]) : console.log('nada');
+  }
+  console.log(passwordArray);
+};
+
 function App() {
   return (
+    <body>
     <div className="App">
       <p>hallo</p>
     </div>
+    </body>
+
   );
 }
 
+
 readWholeDB();
+searchPasswordByTitle("Test Account 1");
 
 export default App;
